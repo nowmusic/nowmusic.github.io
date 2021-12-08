bf2d0717f6af0296fe93baecd975a97bfad766ed
yoonseo <a5014919@naver.com>
2021-12-08 18:53:04 +0900

    기분, youtube 링크로 연결

--- src/main.js
+++ src/main.js
@@ -1,19 +1,372 @@
 function Main() {
-    return `<a class="btn" href="#home_first">테스트 시작하기</a>`;
+    return `<a class="btn" href="#start">시작하기</a>`;
 }
 
 function First() {
-    return `<a class="btn" href="#home_second">fisrt함수실행</a>`;
+    return `<a>당신의 현재 기분은 어떤가요?</a>
+            <a class="btn" href="#good">좋아요</a><br>
+            <a class="btn" href="#normal">보통이에요</a><br>
+            <a class="btn" href="#bad">나빠요</a>`;
 
 }
 function Second(){
-    return `<a class="btn" href="#home_third">second함수실행</a>`;
+    return `<a class="btn" href="#good_happy">행복해요</a><br>
+            <a class="btn" href="#good_excited">신나요</a><br>
+            <a class="btn" href="#good_pitapat">두근두근</a><br>
+            <a class="btn" href="#good_pleased">기뻐요</a><br>
+            <a class="btn" href="#good_proud">뿌듯해요</a><br>
+            <a class="btn" href="#good_relaxed">여유로워요</a>`;
 }
-function Third() {
-    return `<a class="btn" href="#home_fourth">third함수실행</a>
-<br><a class="btn" href="#home_fourth_2">third_2함수실행</a>`;
+
+function Third(){
+    return `<a class="btn" href="#normal_soso">소소해요</a><br>
+            <a class="btn" href="#normal_sleep">잠 깨고 싶어요</a><br>
+            <a class="btn" href="#normal_miss">그리워요</a><br>
+            <a class="btn" href="#normal_upndown">오락가락</a><br>
+            <a class="btn" href="#normal_lonely">외로워요</a><br>
+            <a class="btn" href="#normal_wantsleep">잠이 안 와요</a><br>
+            <a class="btn" href="#normal_busy">바빠요</a>`;
 }
-function Fourth(){
+
+function Fourth() {
+    return `<a class="btn" href="#bad_bummed">꿀꿀해요</a><br>
+            <a class="btn" href="#bad_sad">슬퍼요</a><br>
+            <a class="btn" href="#bad_cry">울고 싶어요</a><br>
+            <a class="btn" href="#bad_hard">힘들어요</a><br>
+            <a class="btn" href="#bad_disappointed">서운해요</a><br>
+            <a class="btn" href="#bad_worried">걱정돼요</a><br>
+            <a class="btn" href="#bad_annoying">짜증나요</a><br>
+            <a class="btn" href="#bad_breakdown">멘붕이에요</a><br>
+            <a class="btn" href="#bad_crazy">미치겠어요</a><br>
+            <a class="btn" href="#bad_bad">별로예요</a><br>
+            <a class="btn" href="#bad_tired">피곤해요</a><br>
+            <a class="btn" href="#bad_crooked">삐뚤어질 거예요</a><br>
+            <a class="btn" href="#bad_comforted">위로받고 싶어요</a>`;
+
+}
+
+function Happy() {
+    return `<div>행복한 당신을 더 행복하게 만들어 줄 플레이리스트</div>
+     <iframe
+    width = "500"
+    height = "305"
+    src = "https://www.youtube.com/embed/FOutKzU3uSc"
+    title = "YouTube video player"
+    frameBorder = "0"
+    allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowFullScreen > < /iframe>;`;
+
+}
+
+function Excited() {
+    return `<div>신날 때 듣는 플레이리스트</div>
+     <iframe
+    width = "500"
+    height = "305"
+    src = "https://www.youtube.com/embed/aoIgrag2rMU"
+    title = "YouTube video player"
+    frameBorder = "0"
+    allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowFullScreen > < /iframe>;`;
+
+}
+
+function Pitapat() {
+    return `<div>두근두근할 때 듣는 노래</div>
+     <iframe
+    width = "500"
+    height = "305"
+    src = "https://www.youtube.com/embed/yqnlSuV5PFA"
+    title = "YouTue video player"
+    frameBorder = "0"
+    allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowFullScreen > < /iframe>;`;
+
+}
+
+function Pleased() {
+    return `<div>기쁠 때 듣는 노래</div>
+     <iframe
+    width = "500"
+    height = "305"
+    src = "https://www.youtube.com/embed/HGrP1qdE5Rw"
+    title = "YouTube video player"
+    frameBorder = "0"
+    allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowFullScreen > < /iframe>;`;
+}
+
+function Proud() {
+    return `<div>뿌듯할 때 듣는 노래</div>
+     <iframe
+    width = "500"
+    height = "305"
+    src = "https://www.youtube.com/embed/SZM-K11FdFA"
+    title = "YouTube video player"
+    frameBorder = "0"
+    allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowFullScreen > < /iframe>;`;
+
+}
+function Relaxed() {
+    return `<div>여유로울 때 듣는 노래</div>
+     <iframe
+    width = "500"
+    height = "305"
+    src = "https://www.youtube.com/embed/iuyG9gbZHFM"
+    title = "YouTube video player"
+    frameBorder = "0"
+    allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowFullScreen > < /iframe>;`;
+
+}
+function SoSo() {
+    return `<div>소소할 때 듣는 노래</div>
+     <iframe
+    width = "500"
+    height = "305"
+    src = "https://www.youtube.com/embed/FKt2k6qTn0k"
+    title = "YouTube video player"
+    frameBorder = "0"
+    allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowFullScreen > < /iframe>;`;
+
+}
+function Sleep() {
+    return `<div>잠 깨고 싶을 때 듣는 노래</div>
+     <iframe
+    width = "500"
+    height = "305"
+    src = "https://www.youtube.com/embed/JNM3I4Q8wGU"
+    title = "YouTube video player"
+    frameBorder = "0"
+    allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowFullScreen > < /iframe>;`;
+
+}
+function Miss() {
+    return `<div>그리울 때 듣는 노래</div>
+     <iframe
+    width = "500"
+    height = "305"
+    src = "https://www.youtube.com/embed/em9Mr4G0-J8"
+    title = "YouTube video player"
+    frameBorder = "0"
+    allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowFullScreen > < /iframe>;`;
+
+}
+function Upndown() {
+    return `<div>기분이 오락가락 때 듣는 노래</div>
+     <iframe
+    width = "500"
+    height = "305"
+    src = "https://www.youtube.com/embed/lbOKEVS7uqA"
+    title = "YouTube video player"
+    frameBorder = "0"
+    allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowFullScreen > < /iframe>;`;
+
+}
+
+function Lonely() {
+    return `<div>외로울 때 듣는 노래</div>
+     <iframe
+    width = "500"
+    height = "305"
+    src = "https://www.youtube.com/embed/W5KZ1fQ22IQ"
+    title = "YouTube video player"
+    frameBorder = "0"
+    allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowFullScreen > < /iframe>;`;
+
+}
+function Wantsleep() {
+    return `<div>자고 싶을 때 듣는 노래</div>
+     <iframe
+    width = "500"
+    height = "305"
+    src = "https://www.youtube.com/embed/vXENF8VSBss"
+    title = "YouTube video player"
+    frameBorder = "0"
+    allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowFullScreen > < /iframe>;`;
+
+}
+function Busy() {
+    return `<div>바쁠 때 듣는 노래</div>
+     <iframe
+    width = "500"
+    height = "305"
+    src = "https://www.youtube.com/embed/lvCNYrIMWWU"
+    title = "YouTube video player"
+    frameBorder = "0"
+    allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowFullScreen > < /iframe>;`;
+
+}
+
+function Bummed() {
+    return `<div>꿀꿀할 때 듣는 노래</div>
+     <iframe
+    width = "500"
+    height = "305"
+    src = "https://www.youtube.com/embed/qvh3eAmAahE"
+    title = "YouTube video player"
+    frameBorder = "0"
+    allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowFullScreen > < /iframe>;`;
+
+}
+function Sad() {
+    return `<div>슬플 때 듣는 노래</div>
+     <iframe
+    width = "500"
+    height = "305"
+    src = "https://www.youtube.com/embed/t_jw-ENr0Jg"
+    title = "YouTube video player"
+    frameBorder = "0"
+    allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowFullScreen > < /iframe>;`;
+
+}
+function Cry() {
+    return `<div>울고 싶을 때 듣는 노래</div>
+     <iframe
+    width = "500"
+    height = "305"
+    src = "https://www.youtube.com/embed/AjiDuZZ_DZU" title="YouTube video player"
+    title = "YouTube video player"
+    frameBorder = "0"
+    allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowFullScreen > < /iframe>;`;
+
+}
+function Hard() {
+    return `<div>힘들 때 듣는 노래</div>
+     <iframe
+    width = "500"
+    height = "305"
+    src = "https://www.youtube.com/embed/nKbBTy7ioxI"
+    title = "YouTube video player"
+    frameBorder = "0"
+    allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowFullScreen > < /iframe>;`;
+
+}
+function Disappointed() {
+    return `<div>서운할 때 듣는 노래</div>
+     <iframe
+    width = "500"
+    height = "305"
+    src = "https://www.youtube.com/embed/6wJepYuiznU"
+    title = "YouTube video player"
+    frameBorder = "0"
+    allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowFullScreen > < /iframe>;`;
+
+}
+function Worried() {
+    return `<div>걱정될 때 듣는 노래</div>
+     <iframe
+    width = "500"
+    height = "305"
+    src = "https://www.youtube.com/embed/EDsPY0d9s4E"
+    title = "YouTube video player"
+    frameBorder = "0"
+    allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowFullScreen > < /iframe>;`;
+
+}
+function Annoying() {
+    return `<div>짜증날 때 듣는 노래</div>
+     <iframe
+    width = "500"
+    height = "305"
+    src = "https://www.youtube.com/embed/q4-2kPQtMbI"
+    title = "YouTube video player"
+    frameBorder = "0"
+    allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowFullScreen > < /iframe>;`;
+
+}
+function Breakdown() {
+    return `<div>멘붕일 때 듣는 노래</div>
+     <iframe
+    width = "500"
+    height = "305"
+    src = "https://www.youtube.com/embed/2HhvS8A0RFQ"
+    title = "YouTube video player"
+    frameBorder = "0"
+    allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowFullScreen > < /iframe>;`;
+
+}
+function Crazy() {
+    return `<div>미치겠을 때 듣는 노래</div>
+     <iframe
+    width = "500"
+    height = "305"
+    src = "https://www.youtube.com/embed/qoxVwvFMrYI"
+    title = "YouTube video player"
+    frameBorder = "0"
+    allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowFullScreen > < /iframe>;`;
+
+}
+function Bad() {
+    return `<div>별로일 때 듣는 노래</div>
+     <iframe
+    width = "500"
+    height = "305"
+    src = "https://www.youtube.com/embed/Mk9Cn-ii6N4"
+    title = "YouTube video player"
+    frameBorder = "0"
+    allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowFullScreen > < /iframe>;`;
+
+}
+function Tired() {
+    return `<div>피곤할 때 듣는 노래</div>
+     <iframe
+    width = "500"
+    height = "305"
+    src = "https://www.youtube.com/embed/HfAhUxPSubs"
+    title = "YouTube video player"
+    frameBorder = "0"
+    allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowFullScreen > < /iframe>;`;
+
+}
+function Crooked() {
+    return `<div>삐뚤어지고 싶을 때 듣는 노래</div>
+     <iframe
+    width = "500"
+    height = "305"
+    src = "https://www.youtube.com/embed/98c28NSJqaw"
+    title = "YouTube video player"
+    frameBorder = "0"
+    allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowFullScreen > < /iframe>;`;
+
+}
+function Comforted() {
+    return `<div>위로받고 싶을 때 듣는 노래</div>
+     <iframe
+    width = "500"
+    height = "305"
+    src = "https://www.youtube.com/embed/ccXj3a0yr2Q"
+    title = "YouTube video player"
+    frameBorder = "0"
+    allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowFullScreen > < /iframe>;`;
+
+}
+
+function Thrd() {
+   return `<a class="btn" href="#home_fourth">third함수실행</a>v<br><a class="btn" href="#home_fourth_2">third_2함수실행</a>`;
+}
+function Foth(){
     return `<div>누구의 어쩌구</div>
 <iframe width="560" height="315" src="https://www.youtube.com/embed/7loLjL5nOHI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
 }
@@ -24,7 +377,37 @@ window.addEventListener('hashchange', () => {
 const router = () => {
     const path = location.hash.substr(1);
     const app = document.querySelector('.app');
-    const route = [{ '': Main }, { home_first: First } , {home_second : Second}, {home_third : Third}, {home_fourth : Fourth}, {home_fourth_2 : Fourth}];
+    const route = [{ '': Main },
+        {start: First } ,
+        {good : Second},
+        {normal : Third},
+        {bad : Fourth},
+        {good_happy : Happy},
+        {good_excited : Excited},
+        {good_pitapat : Pitapat},
+        {good_pleased : Pleased},
+        {good_proud : Proud},
+        {good_relaxed : Relaxed},
+        {normal_soso : SoSo},
+        {normal_sleep : Sleep},
+        {normal_miss : Miss},
+        {normal_upndown : Upndown},
+        {normal_lonely : Lonely},
+        {normal_wantsleep : Sleep},
+        {bad_bummed : Bummed},
+        {bad_sad : Sad},
+        {bad_cry : Cry},
+        {bad_hard : Hard},
+        {bad_disappointed : Disappointed},
+        {bad_worried : Worried},
+        {bad_annoying : Annoying},
+        {bad_breakdown : Breakdown},
+        {bad_crazy : Crazy},
+        {bad_bad : Bad},
+        {bad_tired : Tired},
+        {bad_crooked : Crooked},
+        {bad_comforted : Comforted}
+];
 
     const target = route.filter(e => Object.keys(e)[0] === path);
 
