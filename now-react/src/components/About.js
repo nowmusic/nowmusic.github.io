946ac59f9377d93ea9d4e75722f0c30c526b79f5
dhrgodms <dhrgodms123@gmail.com>
2024-11-24 01:23:27 +0900

    docs: modify about page

--- now-react/src/components/About.js
+++ now-react/src/components/About.js
@@ -9,13 +9,9 @@ export default function About() {
       </div>
       <div class="article">
         <p>
-          <img class="selfie" alt="NowMusic" src={Logo}></img>
-        </p>
-
-        <p>
           <strong>NOWMUSIC</strong>은 SW캡스톤디자인 프로젝트를 위해 제작된
-          웹페이지이다. 사용자의 그날그날 기분에 맞춰 음악 추천을 해 주는
-          사이트이다.
+          웹페이지로, 사용자의 그날그날 기분에 맞춰 음악 추천을 해 주는
+          사이트입니다.
         </p>
       </div>
     </div>
